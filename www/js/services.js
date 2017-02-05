2b0db60ba91a445bc9317be6ef4443be04f6593e
plopushin <lopushin@ualberta.ca>
2017-02-05 11:07:03 -0700

    First commit: Set up template, able to skip or favorite songs with animation, able to add to favorites tab

--- www/js/services.js
+++ www/js/services.js
@@ -1,1 +1,17 @@
-angular.module('songhop.services', []);
+angular.module('songhop.services', [])
+	.factory('User', function() {
+		var o = {
+			favorites: []
+		}
+
+		o.addSongToFavorites = function(song) {
+			//ensure there is a song to add
+			if(!song) return false;
+
+			//otherwise, add to favorites array
+			//unshift adds to beginning of the array
+			o.favorites.unshift(song);
+		}
+
+		return o;
+	});
