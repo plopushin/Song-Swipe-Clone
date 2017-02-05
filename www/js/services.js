bb3a4a49b1ec15bd909dc6e05e75503952c670fa
plopushin <lopushin@ualberta.ca>
2017-02-05 14:49:31 -0700

    add ability for next image to load prior to viewing

--- www/js/services.js
+++ www/js/services.js
@@ -2,7 +2,7 @@ angular.module('songhop.services', [])
 	.factory('User', function() {
 		var o = {
 			favorites: []
-		}
+		};
 
 		o.addSongToFavorites = function(song) {
 			//ensure there is a song to add
@@ -22,4 +22,32 @@ angular.module('songhop.services', [])
 		}
 
 		return o;
-	});
+	})
+
+	.factory('Recommendations', function($http, SERVER) {
+		var o = {
+			queue: []
+		};
+
+		o.getNextSongs = function() {
+			return $http({
+				method: 'GET',
+				url: SERVER.url + '/recommendations'
+			}).success(function(data){
+				//merge data into the queue
+				o.queue = o.queue.concat(data);
+			});
+		}
+
+		o.nextSong = function() {
+			//pop the index 0 off
+			o.queue.shift();
+
+			//if queue is low, fill it up
+			if (o.queue.length <= 3) {
+				o.getNextSongs();
+			}
+		}
+
+		return o;
+	})
