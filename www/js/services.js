87f2595dc781291dfb9039bb700a0b3aa23e9a51
plopushin <lopushin@ualberta.ca>
2017-02-07 16:04:45 -0700

    Methods in place to ensure non-authenticated user can't access core parts of the app

--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,5 @@
-angular.module('songhop.services', [])
-	.factory('User', function($http, SERVER) {
+angular.module('songhop.services', ['ionic.utils'])
+	.factory('User', function($http, $q, $localstorage, SERVER) {
 		var o = {
 			username: false,
 			session_id: false,
@@ -19,9 +19,56 @@ angular.module('songhop.services', [])
 				authRoute = 'login'
 			}
 
-			return $http.post(SERVER.url + '/' + authRoute, {username: username});
+			return $http.post(SERVER.url + '/' + authRoute, {username: username})
+				.success(function(data){
+					o.setSession(data.username, data.session_id, data.favorites);
+				});
 			}
 		
+		//set session data
+		o.setSession = function(username, session_id, favorites) {
+			if (username) o.username = username;
+			if (session_id) o.session_id = session_id;
+			if (favorites) o.favorites = favorites;
+
+			//set data in localstorage object
+			$localstorage.setObject('user', {username: username, session_id: session_id});
+		}
+
+		//check if there is a user session present
+
+		o.checkSession = function() {
+			var defer = $q.defer();
+
+			if (o.session_id) {
+				//if this is already initialized in the service
+				defer.resolve(true);
+
+			} else {
+				//check if there is a session in localstorage from previous use
+				//if so, pull into our service
+
+				var user = $localstorage.getObject('user');
+
+				if (user.username) {
+					//if there's a user, grab favorites from the server
+					o.setSession(user.username, user.session_id);
+					o.populateFavorites().then(function() {
+						defer.resolve(true);
+					});
+				}
+			}
+		}
+
+		//wip out the session data - aka logout
+
+		o.destroySession = function() {
+			$localstorage.setObject('user', {});
+			o.username = false;
+			o.session_id = false;
+			o.favorites = [];
+			o.newFavorites = 0;
+		}
 
 		o.addSongToFavorites = function(song) {
 			//ensure there is a song to add
